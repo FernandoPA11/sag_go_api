580f9cb9eff4b9896adba307cedaf5d17210add9
FernandoPA11 <f.p.a._1234@hotmail.com>
2024-06-21 22:09:40 -0600

    chore: Add database connection and migration functions in main.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"SAG_GO_API/core/db"
+	"SAG_GO_API/pkg/resources"
+	"SAG_GO_API/pkg/routes"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -8,9 +11,12 @@ import (
 
 func main() {
 
+	db.DBconnect()
+	migration()
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", HomeHandler)
+	AddRoutes(router)
 
 	http.ListenAndServe(":3000", router)
 
@@ -18,4 +24,18 @@ func main() {
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
+}
+
+func AddRoutes(router *mux.Router) {
+	routes.UserRouteHandlers(router)
+}
+
+func migration() {
+	db.DB.AutoMigrate(&resources.User{})
+	db.DB.AutoMigrate(&resources.Employee{})
+	db.DB.AutoMigrate(&resources.DocumentEmployee{})
+	db.DB.AutoMigrate(&resources.Role{})
+	db.DB.AutoMigrate(&resources.Permission{})
+	db.DB.AutoMigrate(&resources.RolePermission{})
+	db.DB.AutoMigrate(&resources.UserPermission{})
 }
--- core/db/db.go
+++ core/db/db.go
@@ -1,1 +1,21 @@
 package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DSN = "host=localhost user=fer password=fer123 dbname=sag port=5433 sslmode=disable"
+var DB *gorm.DB
+
+func DBconnect() {
+	var error error
+	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	if error != nil {
+		log.Fatalf("Failed to connect to database! %v", error)
+	} else {
+		println("Connected to database!")
+	}
+}
